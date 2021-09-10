f3841ed070453c14ac682ddb05519a1ce0d1a153
Vinoop K S <33143132+vinoopks@users.noreply.github.com>
2021-09-10 21:33:46 +0530

    fixing typo

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	assertCorrctMessage := func(t testing.TB, got, want string) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
 		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -13,25 +13,25 @@ func TestHello(t *testing.T) {
 	t.Run("to a person", func(t *testing.T) {
 		got := Hello("Vinoop", "")
 		want := "Hello, Vinoop"
-		assertCorrctMessage(t, got, want)
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string", func(t *testing.T) {
 		got := Hello("", "")
 		want := "Hello, World"
-		assertCorrctMessage(t, got, want)
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("in Spanish", func(t *testing.T) {
 		got := Hello("Vinoop", "Spanish")
 		want := "Hola, Vinoop"
-		assertCorrctMessage(t, got, want)
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("in French", func(t *testing.T) {
 		got := Hello("Vinoop", "French")
 		want := "Bonjour, Vinoop"
-		assertCorrctMessage(t, got, want)
+		assertCorrectMessage(t, got, want)
 	})
 
 }
